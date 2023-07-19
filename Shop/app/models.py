e9d030fa4845041f9305cb4ec54f5b70b4e63c04
AndreyVitovtov <andrey.vitovtov@gmail.com>
2023-07-19 15:34:03 +0300

    added models Customer & Cart

--- Shop/app/models.py
+++ Shop/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Customer(models.Model):
+    email = models.TextField(null=False, default='')
+    password = models.TextField(null=False, default='')
+    name = models.TextField(null=False, default='')
+
+
+class Cart(models.Model):
+    count = models.IntegerField(default=1)
+    added = models.DateTimeField(default=timezone.now)
+    customer_id = models.IntegerField(null=False)
+    product_id = models.IntegerField(null=False)
