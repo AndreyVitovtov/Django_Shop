72c6060744890332e85cfeb06f0a99d23e3fcc16
AndreyVitovtov <andrey.vitovtov@gmail.com>
2023-07-20 12:44:05 +0300

    added markup for products

--- Shop/app/views.py
+++ Shop/app/views.py
@@ -1,12 +1,102 @@
+import os
+
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Cart
 
-
 # Create your views here.
+
+# TODO: В home нужно передать:
+#  categories - все категории из БД
+#  selected_category - id первой категории
+#  products - продукты из модели Product по выбранной категории (selected_category)
+
+products = [
+    {
+        'id': 1,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8.webp'
+    },
+    {
+        'id': 2,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8.webp'
+    },
+    {
+        'id': 3,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8.webp'
+    },
+    {
+        'id': 4,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8.webp'
+    },
+    {
+        'id': 5,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8asd.webp'
+    },
+    {
+        'id': 6,
+        'title': 'Notebook Acer Aspire 7 A715-42G-R3EZ',
+        'description': 'Description product',
+        'price': '1000',
+        'category_id': 2,
+        'img': '8d261abc4c96bfbde4cb9d117e63ffc8.webp'
+    },
+]
+
+
+# Checks if there is a product image, if not, replaces it 'no-image.png'
+def images_exists(product):
+    print(os.path.join(settings.STATICFILES_DIRS[0], 'images',
+                       product['img']))
+    product['img'] = product['img'] if os.path.exists(os.path.join(settings.STATICFILES_DIRS[0], 'images',
+                                                                   product['img'])) else 'no-image.png'
+    return product
+
+
 def home(req):
-    return HttpResponse(render(req, 'home.html'))
+    return HttpResponse(render(req, 'home.html', {
+        'categories': [
+            {
+                'id': 1,
+                'title': 'Computers'
+            },
+            {
+                'id': 2,
+                'title': 'Phones'
+            },
+            {
+                'id': 3,
+                'title': 'Household appliances'
+            },
+            {
+                'id': 4,
+                'title': 'Goods for gamers'
+            }
+        ],
+        'selected_category': 2,
+        'products': list(map(images_exists, products))
+    }))
 
 
 def cart(req):
